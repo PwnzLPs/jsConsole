0d8d6747090aa3a21259f28cdf0aa55f76414cf4
Ryan <pwnzlps@gmail.com>
2018-04-20 09:55:08 -0400

    Create commands.js

---
+++ commands.js
@@ -0,0 +1,25 @@
+// Commands for the terminal to interpret
+
+var commands = {
+  login: function() {
+};
+
+handle_command(command) {
+  switch(command) {
+    case login:
+      con.log
+}
+
+var users = [{
+  name: 'sudo',
+  pass: 'root',
+  showname: 'Root User'
+}, {
+  name: 'user',
+  pass: 'password',
+  showname: 'User'
+}, {
+  name: 'ryan',
+  pass: 'password',
+  showname: 'Ryan'
+};
